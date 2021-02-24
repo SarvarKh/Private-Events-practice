507db118cd51ddbfc2caf85b3a21dae65f1eedda
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-25 01:17:46 +0500

    Add sign in and out for user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_name(params[:name])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_path, notice:
+      "Welcome #{user.id}, you successfully Signed In!"
+    else
+      render "new", alert:
+      "User ID and/or Password Invalid!"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "You're now signed out!"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
+  resources :sessions
+  root "users#index"
   resources :users
 end
