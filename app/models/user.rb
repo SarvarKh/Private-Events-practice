9291e4907cca1c6279f3f118b0d38ab79cb259e9
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-26 16:16:00 +0500

    Add associations via foreign key creator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    
+    has_many :events
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_182738) do
+ActiveRecord::Schema.define(version: 2021_02_25_193725) do
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
