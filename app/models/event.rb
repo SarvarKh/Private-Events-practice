a88d5edc7f41e03d376a312a6c986d8c2612ebfe
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-26 20:13:27 +0500

    Fix bug in associations via foreign key creator

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,3 @@
 class Event < ApplicationRecord
-    
-    belongs_to :creator, class_name: "User", foreign_key: "creator_id"
+    belongs_to :creator, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
 class User < ApplicationRecord
-    
-    has_many :events
+    has_many :events, foreign_key: :creator_id
 end
