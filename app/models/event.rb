9f8d367ddf544348255d0e167b9d9a1ae7339795
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-03-04 15:00:29 +0500

    Fix bug in has_many relationship

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
 
     has_many :attendances, foreign_key: :attended_event_id
-    has_many :attended_events, through: :attendances
+    has_many :attendees, through: :attendances
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_many :events, foreign_key: :creator_id
     
     has_many :attendances, foreign_key: :attendee_id
-    has_many :attendees, through: :attendances
+    has_many :attended_events, through: :attendances
 end
